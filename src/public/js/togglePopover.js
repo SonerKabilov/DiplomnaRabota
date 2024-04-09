cf602dcc179915f4c0c6e9858e2a5aa23fb36891
SonerKabilov <sonerkabilov@abv.bg>
2024-04-09 23:52:58 +0300

    Added styles and fixed js functionality for popovers

--- src/public/js/togglePopover.js
+++ src/public/js/togglePopover.js
@@ -1,44 +1,49 @@
 const container = document.querySelector('.lessons');
 
 const showPopover = () => {
-    container.addEventListener('click', function(event) {
+    container.addEventListener('click', function (event) {
         const targetButton = event.target.closest('i');
-        console.log(targetButton)
-    
+
         if (targetButton) {
             const popovers = document.querySelectorAll(".popover");
             let popover = null;
-    
+
             for (const p of popovers) {
                 if (p.parentNode === targetButton.parentNode) {
                     popover = p;
                 }
             }
-    
-            if (popover) {
-                const rect = targetButton.getBoundingClientRect();
-                const top = rect.top + window.scrollY + targetButton.offsetHeight;
-                const left = rect.left + window.scrollX + (targetButton.offsetWidth - popover.offsetWidth) / 2;
-    
-                popover.style.top = `${top}px`;
-                popover.style.left = `${left}px`;
-                popover.style.display = 'block';
 
-                const hidePopover = () => {
+            if (popover) {
+                const computerStyle = window.getComputedStyle(popover);
+                const displayPropertyValue = computerStyle.getPropertyValue('display');
+
+                if (displayPropertyValue === 'block') {
                     popover.style.display = 'none';
-                    targetButton.removeEventListener('click', hidePopover);
-                }
-        
-                targetButton.addEventListener('click', hidePopover);
-    
-                const hideAllPopovers = (e) => {
-                    if (!popover.contains(e.target) && e.target !== targetButton) {
-                        popover.style.display = 'none';
-                        document.removeEventListener('click', hideAllPopovers);
+                } else if (displayPropertyValue === 'none') {
+                    
+                    const hideAllPopovers = (e) => {
+                        if (!popover.contains(e.target) && e.target !== targetButton) {
+                            popover.style.display = 'none';
+                            document.removeEventListener('click', hideAllPopovers);
+                        }
                     }
+
+                    document.addEventListener('click', hideAllPopovers);
+
+                    const rect = targetButton.getBoundingClientRect();
+                    const top = rect.top + window.scrollY + targetButton.offsetHeight;
+                    const left = rect.left + window.scrollX + (targetButton.offsetWidth - popover.offsetWidth) / 2;
+
+                    popover.style.top = `${top}px`;
+                    popover.style.left = `${left}px`;
+                    popover.style.display = 'block';
+                    popover.style.opacity = 0;
+
+                    setTimeout(() => {
+                        popover.style.opacity = 1;
+                    }, 50);
                 }
-                
-                document.addEventListener('click', hideAllPopovers);
             }
         }
     });
